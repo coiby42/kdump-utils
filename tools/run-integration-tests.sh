97b971f8efe8192c6c44e60247c8b5b06c2e9e48
Coiby Xu <coxu@redhat.com>
2022-04-15 18:15:17 +0800

    tests: enable Github action
    With this patch, four kinds of tests are triggered when there is a
    pull request created in a Github kexec-tools repo,
       - format check using shfmt
       - static analysis using shellcheck
       - ShellSpec unit tests (test cases in spec/)
       - integration tests (tests cases in tests/)
    
    Signed-off-by: Coiby Xu <coxu@redhat.com>

---
+++ tools/run-integration-tests.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -ex
+
+[[ -d ${0%/*} ]] && cd "${0%/*}"/../
+
+VERSION=$(rpmspec -q --queryformat "%{VERSION}" kdump-utils.spec)
+SRC_ARCHIVE=kdump-utils-$VERSION.tar.gz
+if ! git archive --format=tar.gz -o "$SRC_ARCHIVE" --prefix=kdump-utils-$VERSION/ HEAD; then
+	echo "Failed to create kdump-utils source archive"
+	exit 1
+fi
+
+if ! rpmbuild -ba -D "dist .fc40" -D "_sourcedir $(pwd)" -D "_builddir $(pwd)" -D "_srcrpmdir $(pwd)" -D "_rpmdir $(pwd)"  kdump-utils.spec; then
+        echo "Failed to build kdump-utils rpm"
+        exit 1
+fi
+
+	echo "Failed to build kdump-utils rpm"
+	exit 1
+fi
+
+arch=$(uname -m)
+rpm_name=$(rpmspec -D "dist .fc40" -q --queryformat '%{NAME}-%{VERSION}-%{RELEASE}.%{ARCH}' kdump-utils.spec)
+rpm_path="$(pwd)/${arch}/${rpm_name}.rpm"
+if [[ ! -f $rpm_path ]]; then
+	echo "Failed to find built kdump-utils rpm ($path doesn't eixst)"
+fi
+
+cd tests && tmt --context custom_mirror=https://mirrors.tuna.tsinghua.edu.cn/fedora run --environment KDUMP_UTILS_RPM="$rpm_path" -a provision -h virtual -c system
+
