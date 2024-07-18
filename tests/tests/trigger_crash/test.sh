0a5edd57bcd6e8afd6e246b2331d6ec20b023ace
Coiby Xu <coxu@redhat.com>
2024-07-18 11:02:23 +0800

    Add Test Management Tool kdump NFS tests
    Currently, all integration tests fail F40 because of failing to install
    package and enable services on the cloud image properly. TMT (Test Management
    Tool) supports running tests on virtual machines and provides an
    user-friendly interface (YAML) to define tasks like installing packages,
    running commands on the virtual machines. By TMT, we no longer need to spend
    time and efforts on maintaining the current test framework and can also
    enjoy handing features like managing tests provided by TMT.
    
    There are six test steps in TMT. Quoting [1], here is an overview,
     - discover
       Gather information about test cases to be executed.
    
     - provision
       Provision an environment for testing or use localhost.
    
     - prepare
       Prepare the environment for testing.
    
     - execute
       Run tests using the specified executor.
    
     - report
       Provide test results overview and send reports.
    
     - finish
       Perform the finishing tasks and clean up provisioned guests.
    
    And each step step is configurable via the so-called plans [2]. TMT also
    uses plans to group relevant tests and select needed tests.
    
    This commit adds the test of sending crash dump to a NFS target by
    setting two VMs using TMT's MultiHost Testing feature [3],
     - client
       Setup up NFS dump target and trigger crash
     - server
       acts as an NFS server and check sent vmcore
    
    The scaffold of TMT test project can be created as follows,
    
        mkdir tests && cd tests
        tmt init --template mini
        mv plans/example/{example,kdump}.fmf
    
        tmt tests create /setup/default_crashkernel -t beakerlib
        tmt tests create /setup/nfs_server -t beakerlib
        tmt tests create /test/trigger_crash -t beakerlib
        tmt tests create /test/check_vmcore -t beakerlib
    
    Then we fill in the content for created plans and tests,
    
        # tmt run -a
        /var/tmp/tmt/run-017
    
        /plans/kdump
            discover
                how: fmf
                name: client-setup
                directory: /root/kdump-tests
                tests: /setup/kdump
                how: fmf
                name: server-setup
        ...
                execute task #4: server-test on server
                how: tmt
    
                summary: 4 tests executed
            report
                how: display
                summary: 4 tests passed
            finish
    
    An main.fmf is put under the root directory of tests and setup for other
    tests to inherit [7].
    
    [1] https://tmt.readthedocs.io/en/stable/overview.html#run
    [2] https://tmt.readthedocs.io/en/stable/spec/plans.html
    [3] https://tmt.readthedocs.io/en/stable/guide.html#multihost-testing
    [7] https://tmt.readthedocs.io/en/stable/guide.html#inheritance
    
    Signed-off-by: Coiby Xu <coxu@redhat.com>

---
+++ tests/tests/trigger_crash/test.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+function get_IP() {
+    if echo $1 | grep -E -q '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+'; then
+        echo $1
+    else
+        host $1 | sed -n -e 's/.*has address //p' | head -n 1
+    fi
+}
+
+function assign_server_roles() {
+    if [ -n "${TMT_TOPOLOGY_BASH}" ] && [ -f ${TMT_TOPOLOGY_BASH} ]; then
+        # assign roles based on tmt topology data
+        cat ${TMT_TOPOLOGY_BASH}
+        . ${TMT_TOPOLOGY_BASH}
+
+        export CLIENT=${TMT_GUESTS["client.name"]}
+        export SERVER=${TMT_GUESTS["server.name"]}
+        MY_IP="${TMT_GUEST['hostname']}"
+    elif [ -n "$SERVERS" ]; then
+        # assign roles using SERVERS and CLIENTS variables
+        export SERVER=$( echo "$SERVERS $CLIENTS" | awk '{ print $1 }')
+        export CLIENT=$( echo "$SERVERS $CLIENTS" | awk '{ print $2 }')
+    fi
+
+    [ -z "$MY_IP" ] && MY_IP=$( hostname -I | awk '{ print $1 }' )
+    [ -n "$SERVER" ] && export SERVER_IP=$( get_IP $SERVER )
+    [ -n "$CLIENT" ] && export CLIENT_IP=$( get_IP $CLIENT )
+}
+
+rlJournalStart
+
+  if [ $TMT_REBOOT_COUNT == 0 ]; then
+    rlPhaseStartSetup
+        assign_server_roles
+        rlLog "SERVER: $SERVER ${SERVER_IP}"
+        rlLog "CLIENT: ${CLIENT} ${CLIENT}"
+        rlLog "This system is: $(hostname) ${MY_IP}"
+        rlRun "echo nfs $SERVER:/var/tmp/nfsshare >> /etc/kdump.conf"
+    rlPhaseEnd
+
+    rlPhaseStartTest
+        rlRun "kdumpctl restart"
+        rlRun "sync"
+        rlRun "echo 1 > /proc/sys/kernel/sysrq"
+        rlRun "echo c > /proc/sysrq-trigger"
+    rlPhaseEnd
+  fi
+rlJournalEnd
